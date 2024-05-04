7056fe46354bf6a7db7b345baaeccad4f4d70cfa
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-05-04 15:34:33 +0530

    Scripts Fixed

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,13 @@
-let likeContainers = document.getElementsByClassName('like-container')
+let likeContainers = document.getElementsByClassName("like-container");
 for (let likeContainer of likeContainers) {
-    if (likeContainer.childElementCount == 2){
-        likeContainer.lastElementChild.style.display = 'none'
-    }
+  if (likeContainer.childElementCount == 2) {
+    likeContainer.lastElementChild.style.display = "none";
+  }
+}
+
+let followButtons = document.getElementsByClassName("follow-button");
+for (let followButton of followButtons) {
+  if (followButton.innerText == "") {
+    followButton.innerText = "Follow";
+  }
 }
